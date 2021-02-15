88532543a55cd377ded84ebd11ac7f2621397aa9
Florentxu <72868902+Florentxu@users.noreply.github.com>
2021-02-15 16:53:44 +0100

    ajout de la connexion MongoDB

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-var bodyParser = require('body-parser')
+
+const app = require('./src/services/express.services');
+const mongoose = require('./src/services/mongodb.services');
+mongoose.connectDB();
+app.start();
 
 
-console.log('app is running');
+// const express = require('express');
+// const fs = require('fs');
+// const app = express();
+// const port = 3000;
+
+// const users = require('./mockup/users.json');
+
+// app.get('/', (req,res)=> {
+//     res.send('Hello world');
+// });
+
+// app.listen(port,() => {
+//     console.log(`app is running on port ${port}`);
+// });
+
+
+// //dossier mockup -> users.json (avec 3 users)
+// // email, firstname, lastname, 
+// // /users accessible en get
+
+// app.get('/users',(req,res)=> {
+//     res.send(users);
+// });
---
+++ src/configs/db.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongo: {
+    url:"mongodb+srv://admin:admin@cluster0.p7bgj.mongodb.net/RepairCafe?retryWrites=true&w=majority",
+  }
+};
